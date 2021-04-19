7b45c4131301e0a4cae733384d3265e1cc79fd0e
natoliveira <nataliadeolive@gmail.com>
2021-04-18 22:20:16 -0300

    implementacao do algoritmo BuscaBinaria

---
+++ cmake-build-debug/CMakeFiles/untitled21.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled21.dir/main.cpp.o"
+  "untitled21"
+  "untitled21.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
